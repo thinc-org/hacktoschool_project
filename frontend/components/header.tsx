8a5e724029aabeb85f60cf56a52db614a349e7da
matikant <fe-2003@hotmail.com>
2023-01-10 20:19:38 +0700

    feat: add half left of header

---
+++ frontend/components/header.tsx
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faArrowRight, faBars, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+export default function Header() {
+    const [isNavVisible, setNavVisible] = useState(false);
+    const [isSmallScreen, setSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 700px)");
+        mediaQuery.addListener(handleMediaQueryChange);
+        // handleMediaQueryChange(mediaQuery);
+
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        }
+    }, [])
+
+    const handleMediaQueryChange = (mediaQuery: MediaQueryListEvent) => {
+        if (mediaQuery.matches) {
+            setSmallScreen(true);
+        }
+        else {
+            setSmallScreen(false);
+        }
+    }
+
+    const toggleNav = () => {
+        setNavVisible(!isNavVisible);
+    }
+
+    return (
+        <header className="header">
+            {(isNavVisible || !isSmallScreen) && (
+                <>
+                    <p className="logo">GlobalTalk</p>
+                    <nav className="nav">
+                        <a href="/" className="topics">Home</a>
+                        <a href="/" className="topics">Textbook</a>
+                        <a href="/" className="topics">Statistics</a>
+                        <div className="dropdown">
+                            <button className="dropbtn">Games <FontAwesomeIcon icon={faChevronDown}/>
+                            </button>
+                            <div className="dropdown-content">
+                                <a href="/">Sprint <FontAwesomeIcon icon={faArrowRight}/></a>
+                                <a href="/">Audio-call <FontAwesomeIcon icon={faArrowRight} /></a>
+                            </div>
+                        </div>
+                    </nav>
+                </>
+            )}
+            <button className="loginbtn">Login</button>
+            <button onClick={toggleNav} className="burger"><FontAwesomeIcon icon={faBars} /></button>
+        </header>
+    )
+}
+// <i className="fa fa-bars"></i>
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/header.css";
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
