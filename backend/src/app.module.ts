9dbd27716043368b76d3d637faa9df9f5d7558f5
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-12 10:45:52 +0700

    build(backend): init mongo database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [     
+    MongooseModule.forRoot(process.env.MONGO_URL,{dbName: process.env.MONGO_NAME})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
