b8f3c5901df49e3282606f7dba54722fa8ce8248
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-12 17:30:46 +0700

    build(backend): API for login

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import { Get, Post ,Body, Controller } from '@nestjs/common';
+import { check } from 'prettier';
+import { CheckUserDto } from 'src/dto/check-user.dto';
 import { CreateUserDto } from 'src/dto/create-user.dto';
 import { UsersDB } from 'src/schemas/usersdb.schema';
 import { UsersService } from './users.service';
@@ -19,6 +21,24 @@ export class UsersController {
         
         return await this.coursesService.createUsersData(createCourseDto);
 
+    }
+
+    @Post('/checkUserExist')
+    async checkUserExists(@Body() checkUserDto: CheckUserDto): Promise<boolean> {
+        return await this.coursesService.checkUsernameExists(checkUserDto);
+    }
+
+    @Post('/checkLogin')
+    async checkLogin(@Body() checkUserDto: CheckUserDto): Promise<string> {
+        if(await this.coursesService.checkUsernameExists(checkUserDto)){
+            if(await this.coursesService.checkPassword(checkUserDto)){
+                return "Logged In";
+            } else {
+                return "Wrong password";
+            }
+        } else {
+            return "Username not found";
+        }
     }
 
 }
