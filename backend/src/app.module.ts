4b325bfb1f094245020bfee767ff85e6c9a48835
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-12 10:57:28 +0700

    build(backend): create collection for users ad courses

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
   imports: [     
-    MongooseModule.forRoot(process.env.MONGO_URL,{dbName: process.env.MONGO_NAME})
+    MongooseModule.forRoot(process.env.MONGO_URL,{dbName: process.env.MONGO_NAME}), UsersModule, CoursesModule
   ],
   controllers: [AppController],
   providers: [AppService],
