c292efc66d5dcb96fb8fdf697c341165c335ce74
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-30 14:25:47 +0700

    fix(frontend): fix home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import { FONT_MANIFEST } from "next/dist/shared/lib/constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Landing from "../components/landing";
@@ -8,6 +8,14 @@ import PagesStyle from "../styles/pages.module.css";
 
 export default function Home() {
   const [isLogin, setLogin] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }, [isLogin]);
   return (
     <>
       <Header></Header>
